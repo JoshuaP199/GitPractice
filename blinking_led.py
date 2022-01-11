40857f447447040b0d3f74dd4558db31fa6edc52
JoshuaP199 <joshperez199@gmail.com>
2022-01-11 17:20:38 -0500

    Added exception handling

--- blinking_led.py
+++ blinking_led.py
@@ -1,44 +1,49 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-RED = 8
-GREEN = 12
-YELLOW = 11
+try:
+    RED = 8
+    GREEN = 12
+    YELLOW = 11
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-    
-GPIO.setup(RED, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(GREEN, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(YELLOW, GPIO.OUT, initial = GPIO.HIGH)
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+        
+    GPIO.setup(RED, GPIO.OUT, initial = GPIO.LOW)
+    GPIO.setup(GREEN, GPIO.OUT, initial = GPIO.LOW)
+    GPIO.setup(YELLOW, GPIO.OUT, initial = GPIO.HIGH)
 
-def led_on(color):
-    GPIO.output(color, GPIO.HIGH)
+    def led_on(color):
+        GPIO.output(color, GPIO.HIGH)
 
-def led_off(color):
-    GPIO.output(color, GPIO.LOW)
-    
-def pauseTicker(pause):
-    for i in range(pause):
-        if i % 2 == 0:
-            GPIO.output(YELLOW, GPIO.HIGH)
-            sleep(1)
-        else:
-            GPIO.output(YELLOW, GPIO.LOW)
-            sleep(1)
+    def led_off(color):
+        GPIO.output(color, GPIO.LOW)
+        
+    def pauseTicker(pause):
+        for i in range(pause):
+            if i % 2 == 0:
+                GPIO.output(YELLOW, GPIO.HIGH)
+                sleep(1)
+            else:
+                GPIO.output(YELLOW, GPIO.LOW)
+                sleep(1)
+        GPIO.output(YELLOW, GPIO.LOW)
+        GPIO.output(RED, GPIO.LOW)
+        GPIO.output(GREEN, GPIO.LOW)
+        sleep(1)
+
     GPIO.output(YELLOW, GPIO.LOW)
-    GPIO.output(RED, GPIO.LOW)
-    GPIO.output(GREEN, GPIO.LOW)
-    sleep(1)
+    sleep(5)
 
-GPIO.output(YELLOW, GPIO.LOW)
-sleep(5)
-
-    
-while True:
-    led_on(GREEN)
-    led_off(RED)
-    pauseTicker(15)
-    led_on(RED)
-    led_off(GREEN)
-    pauseTicker(15)
+        
+    while True:
+        led_on(GREEN)
+        led_off(RED)
+        pauseTicker(15)
+        led_on(RED)
+        led_off(GREEN)
+        pauseTicker(15)
+except KeyboardInterrupt:
+    print("Stopped")
+finally:
+    GPIO.cleanup()
