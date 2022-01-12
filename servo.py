a4d99157ba57bb503e196a08ebab051547ecf6cf
JoshuaP199 <joshperez199@gmail.com>
2022-01-12 12:35:46 -0500

    trimming

--- servo.py
+++ servo.py
@@ -9,8 +9,21 @@ GPIO.setup(servoPIN, GPIO.OUT)
 p = GPIO.PWM(servoPIN, 50) #Pin 11 for PWM with 50hz
 p.start(2.5) #Initialization
 
+def move(degree, time):
+    p.ChangeDutyCycle(degree)
+    sleep(time)
+
 try:
     while True:
+        move(5, 1)
+        move(7.5, 1)
+        move(10, 1)
+        move(12.5, 2)
+        move(10, 1)
+        move(7.5, 1)
+        move(5, 1)
+        move(2.5, 1)
+        '''
         p.ChangeDutyCycle(5)
         sleep(1)
         p.ChangeDutyCycle(7.5)
@@ -27,7 +40,8 @@ try:
         sleep(1)
         p.ChangeDutyCycle(2.5)
         sleep(1)
-except KeyboardInterrupt:
+        '''
+except KeyboardInterrupt: #ctrl+c
     p.ChangeDutyCycle(2.5)
     sleep(1)
     p.stop()
